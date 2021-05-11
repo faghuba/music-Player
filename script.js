a868c54734a5c073e3efe6e3c3d09addddee8b89
faghuba <techaghuba@gmail.con>
2021-05-11 21:17:49 +0300

    Ui-Build

---
+++ script.js
@@ -0,0 +1,7 @@
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const playBtn = document.getElementById('play');
+
+
+
