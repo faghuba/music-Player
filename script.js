b1988ac1695bfae10ecfffa4ca3ba12b3c6ea2a8
faghuba <techaghuba@gmail.con>
2021-05-12 14:51:09 +0300

    styling

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const music = document.querySelector('audio');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const playBtn = document.getElementById('play');
-
-
-
+alert('object object')
